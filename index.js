980ec57a18c8801ee534eacaeb24060a1efb6f74
kimducanh27 <kimducanh27@gmail.com>
2026-02-26 15:19:30 +0700

    update thêm tính năng mới

--- index.js
+++ index.js
@@ -834,6 +834,8 @@ const CONFIG = {
     MAX_BACKUP_SIZE_MB: 10,          // Giới hạn file backup (Discord thường: 10MB, Nitro: 50-100MB)
     // Telegram Backup Config
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+    TELEGRAM_API_ID: process.env.TELEGRAM_API_ID,
+    TELEGRAM_API_HASH: process.env.TELEGRAM_API_HASH,
     TELEGRAM_BACKUP_CHAT_ID: "7155481287",     // Điền Chat ID nhận file vào đây
     // Addon HTTP Server
     ADDON_HTTP_PORT: 8080,
